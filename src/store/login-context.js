97ed684a83fe1c8bc77d24d5aaac967225d4a3ec
IqraMomin <iqra2018momin@gmail.com>
2025-11-03 04:19:53 +0530

    AutoLogout_updated

--- src/store/login-context.js
+++ src/store/login-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 
 
 
@@ -8,28 +8,62 @@ const LoginContext = React.createContext({
     login:(token)=>{},
     logout:()=>{}
 })
+let logoutTimer;
+const calculateRemainingTime = (time)=>{
+    const currentTime = new Date().getTime();
+    const expirationTime = new Date(time).getTime();
+    const remainingDuration = expirationTime - currentTime;
+    return remainingDuration;
+}
+
+
 
 export const LoginProvider = (props)=>{
 
-    const idToken = JSON.parse(localStorage.getItem("idToken")) || "";
+    const idToken = localStorage.getItem("idToken") || "";
 
     const [token,setToken] = useState(idToken);
 
+    
     const isLoggedIn = !!token;
 
     const loginHandler = (token)=>{
-        setTimeout(()=>{
-            localStorage.removeItem("idToken");
-            setToken(null);
-        },5000)
-        localStorage.setItem("idToken",JSON.stringify(token));
         setToken(token);
+        const expirationTime = new Date(new Date().getTime()+5*60*1000);
+        localStorage.setItem("idToken",token);
+        localStorage.setItem("expiration",expirationTime.toISOString());
+
+        const remaining = calculateRemainingTime(expirationTime);
+        logoutTimer = setTimeout(logoutHandler,remaining);
+        
     }
     const logoutHandler = ()=>{
-        localStorage.removeItem("idToken");
         setToken(null);
+        localStorage.removeItem("idToken");
+        localStorage.removeItem("expiration");
+        if(logoutTimer){
+            clearTimeout(logoutTimer);
+        }
+        
 
     }
+    useEffect(()=>{
+        const storedToken = localStorage.getItem("idToken");
+        const storedExpirationTime = localStorage.getItem("expiration");
+    
+        if(storedToken && storedExpirationTime){
+            const remainingTime = calculateRemainingTime(storedExpirationTime);
+            if(remainingTime<=0){
+                logoutHandler();
+            }else{
+                setToken(storedToken);
+                logoutTimer = setTimeout(logoutHandler,remainingTime);
+            }
+        } 
+    
+        
+    
+    },[])
 
     const contextValue = {
         token:token,
