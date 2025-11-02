7d33a4a0359fab0208714f836fb8f637cbca8d64
IqraMomin <iqra2018momin@gmail.com>
2025-11-03 02:45:54 +0530

    ValidatingToken

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 import LoginContext from './store/login-context';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 function App() {
   const loginCtx= useContext(LoginContext);
+
+useEffect(()=>{
+  const checkToken = async()=>{
+    if(!loginCtx.token) return;
+  try{
+    const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyAPItEdIRymlHllXc5EWozfIIUZ3q4TTOY",{
+      method:"POST",
+      body:JSON.stringify({idToken:loginCtx.token}),
+      headers:{
+        "Content-type":"application/json"
+      }
+    })
+    if(!response.ok){
+      loginCtx.logout();
+    }
+
+  }catch{
+    loginCtx.logout();
+  }
+
+  }
+  checkToken();
+  
+},[loginCtx]);
+
   return (
     <Layout>
       <Switch>
